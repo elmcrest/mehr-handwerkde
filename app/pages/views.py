e9d581e69d31dd322ae319bc47d4374b819efc70
Marius Räsener <marius@raesener.de>
2020-02-20 11:49:19 +0100

    switch workplace

--- app/pages/views.py
+++ app/pages/views.py
@@ -1,6 +1,12 @@
 from django.views.generic import TemplateView
+from projects.models import Project
 
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx["highlights"] = Project.objects.filter(highlight=True)
+        return ctx
 
--- app/projects/models.py
+++ app/projects/models.py
@@ -3,6 +3,8 @@ from martor.models import MartorField
 
 
 class Project(models.Model):
+    published = models.BooleanField("Veröffentlicht", default=False)
+    highlight = models.BooleanField("Highlight", default=False)
     name = models.CharField("Name", help_text="Ein schöner Projektname.", max_length=30)
     date = models.DateField("Datum")
     content = MartorField("Inhalt", help_text="Markdown Quelltext")
